9743edcaf169b297b6c7135ce3351ef6898d154f
Karol Swieca <swieca.karol@gmail.com>
2017-10-17 20:31:49 +0200

    Add meetup form

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,7 +1,112 @@
+
+
 <template>
-  <div>
-    <p>
-      The create meetup page
-    </p>
-  </div>
+
+<v-container class="mt-1">
+    <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+            <h4 class="primary--text">Create new meetup</h4>
+        </v-flex>
+    </v-layout>
+    <v-layout row>
+        <v-flex xs12>
+            <form @submit.prevent="onCreateMeetup">
+              <v-layout row>
+                <v-flex xs12 sm6 offset-sm3>
+                  <v-text-field
+                    name="title"
+                    label="Title"
+                    id="title"
+                    v-model="title"
+                    required></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12 sm6 offset-sm3>
+                  <v-text-field
+                    name="location"
+                    label="Location"
+                    id="location"
+                    v-model="location"
+                    required></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12 sm6 offset-sm3>
+                  <v-text-field
+                    name="imageUrl"
+                    label="Image URL"
+                    id="image-url"
+                    v-model="imageUrl"
+                    required></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12 sm6 offset-sm3>
+              <img style="max-width:300px" :src="imageUrl" />
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12 sm6 offset-sm3>
+                  <v-text-field
+                    name="description"
+                    label="Description"
+                    id="description"
+                    v-model="description"
+                    multi-line
+                    required></v-text-field>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex xs12 sm6 offset-sm3>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    class="primary"
+                    :disabled="!formIsValid"
+                    type="submit">Create Meetup</v-btn>
+                </v-flex>
+              </v-layout>
+            </form>
+        </v-flex>
+    </v-layout>
+</v-container>
+
 </template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        title: '',
+        location: '',
+        imageUrl: '',
+        description: ''
+      }
+    },
+    computed: {
+      formIsValid () {
+        return this.title !== '' &&
+        this.location !== '' &&
+        this.description !== '' &&
+        this.imageUrl !== ''
+      }
+    },
+    methods: {
+      onCreateMeetup () {
+        if (!this.formIsValid) {
+          return
+        }
+        const meetupData = {
+          title: this.title,
+          location: this.location,
+          imageUrl: this.imageUrl,
+          description: this.description,
+          date: new Date()
+        }
+        this.$store.dispatch('createMeetup', meetupData)
+        this.$router.push('/meetups')
+      }
+    }
+  }
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -20,8 +20,25 @@ export const store = new Vuex.Store({
       registeredMeetups: ['asdasd2']
     }
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
+  },
+  actions: {
+    createMeetup ({commit}, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: 'ndasjskjdnk2'
+      }
+      // Reach out to firebase and store it
+      commit('createMeetup', meetup)
+    }
+  },
   getters: {
     loadedMeetups (state) {
       return state.loadedMeetups.sort((meetupA, meetupB) => {
