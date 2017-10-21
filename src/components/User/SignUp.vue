280dd137cc7fe16bec037cfeeced2fbf41a6eec5
Karol Swieca <swieca.karol@gmail.com>
2017-10-21 19:34:52 +0200

    sign up form template

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,7 +1,88 @@
+
+
 <template>
-  <div>
-    <p>
-      The sign up page
-    </p>
-  </div>
+
+<v-container>
+    <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+          <v-card>
+            <v-card-text>
+              <v-container>
+                <form>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      type="email"
+                      v-model="email"
+                      required>
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      type="password"
+                      v-model="password"
+                      required>
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      type="password"
+                      v-model="confirmPassword"
+                      :rules="[comparePasswords]"
+                      >
+                      </v-text-field>
+                    </v-flex>
+                  </v-layout>
+                  <v-layout row>
+                    <v-flex xs12>
+                      <v-btn
+                        class="primary"
+                        type="submit">Sign up</v-btn>
+                    </v-flex>
+                  </v-layout>
+                </form>
+              </v-container>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+    </v-layout>
+</v-container>
+
 </template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+    }
+  },
+  methods: {
+    onSignup () {
+      // Vuex
+      console.log({email: this.email, pass: this.password, compPass: this.confirmPassword})
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -17,5 +18,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA1-aKXUHlUXjM8Mgc-4nESFpEAqqHcB_I',
+      authDomain: 'vue-meetup-yt.firebaseapp.com',
+      databaseURL: 'https://vue-meetup-yt.firebaseio.com',
+      projectId: 'vue-meetup-yt',
+      storageBucket: 'vue-meetup-yt.appspot.com'
+    })
+  }
 })
